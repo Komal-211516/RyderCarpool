48dd825c09e2bf14368588635cfa7b20c373a7be
komalkumari <komalkumari1621@gmail.com>
2025-10-17 15:51:40 +0000

    Initial commit: save current RyderCarpoolApp work

---
+++ app/src/main/java/com/rydercarpool/MainActivity.kt
@@ -0,0 +1,13 @@
+cat > MainActivity.kt << 'EOF'
+package com.rydercarpool
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+EOF
